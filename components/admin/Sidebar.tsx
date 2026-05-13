4ee41df4914f1f894d5e0b00108473c9806b9c96
KomalBro <komalbro22@gmail.com>
2026-05-13 14:34:26 +0530

    Fix ESLint error: specify icon type and add React import

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -1,4 +1,5 @@
 'use client'
+import React from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import {
@@ -20,7 +21,7 @@ import {
 
 interface SidebarLink {
     href: string
-    icon: any
+    icon: React.ElementType
     label: string
 }
 
